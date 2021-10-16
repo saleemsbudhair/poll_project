e417675013cd407a1e34c45bfd04b5ded67a1fcd
saleem <saleem.abudheer@iris.ps>
2021-10-16 22:23:39 +0300

    Fix collect static

--- poll_project/settings.py
+++ poll_project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-89u0#)mewg-dkb$09ip22oir6*%vgz-nc5^83cz%^o+^ksht3d
 DEBUG = os.environ.get('DEBUG', True)
 
 ALLOWED_HOSTS = ['*']
-STATIC_ROOT = BASE_DIR / "static"
+STATIC_ROOT = "static"
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / "poll_project/static",
